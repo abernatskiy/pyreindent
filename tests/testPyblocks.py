b8c2e896837c017a4c7b2450d1594b9b4ce057cb
Anton Bernatskiy (kohama) <abernats@uvm.edu>
2020-02-14 23:54:21 -0500

    A somewhat more orderly test for pyblocks
    	modified:   tests/testPyblocks.py

--- tests/testPyblocks.py
+++ tests/testPyblocks.py
@@ -1,7 +1,18 @@
-#from pyblocks import quoteStatus
 import pyblocks
 
-print(pyblocks.quoteStatus("   # fdsfs", None))
-print(pyblocks.quoteStatus("   dsds ''' ''' # '''", None))
-print(pyblocks.quoteStatus("   dsds ''' ''' # '''", "'''"))
-print(pyblocks.quoteStatus("   dsds ''' ''' # '''", '"""'))
+def testQuoteStatus(qs):
+	mappings = [ ( ("   # fdsfs", None), ('   __comment__', None) ),
+	             ( ("   dsds ''' ''' # '''", None), ('   dsds __stringLiteral__ __comment__', None) ),
+	             ( ("   dsds ''' ''' # '''", "'''"), ('__stringLiteral__ __stringLiteral__', None) ),
+	             ( ("   dsds ''' ''' # '''", '"""'), ('__stringLiteral__', '"""') ) ]
+	for (line, curQuote), output in mappings:
+		try:
+			assert(qs(line, curQuote) == output)
+		except AssertionError:
+			raise AssertionError(f'testQuoteStatus: quote status function couldnt map {(line, curQuote)} -> {output}')
+
+def testSplitIntoBlocks(sib):
+	raise NotImplementedError
+
+testQuoteStatus(pyblocks.quoteStatus)
+testSplitIntoBlocks(pyblocks.splitIntoBlocks)
